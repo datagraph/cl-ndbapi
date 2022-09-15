645b53d6d337b41976ffa94877b54492c794303d
Max-Gerd Retzlaff <mgr@matroid.org>
2022-09-15 13:46:13 +0200

    add build-ndbapi.sh

---
+++ build-ndbapi.sh
@@ -0,0 +1,8 @@
+swig3.0  -v -cpperraswarn -cffi -noswig-lisp -includeall -I/mnt/ssd/home/rondb/code/rondb/storage/ndb/include/ -I/mnt/ssd/home/rondb/code/rondb/prod_build/storage/ndb/include/ -I/mnt/ssd/home/rondb/code/rondb/storage/ndb/include/ndbapi/ -c++ -module ndbapi ndbapi.i 
+
+# fix clos file
+temp="$(tempfile)"
+clos="ndbapi-clos.lisp"
+cat $clos > $temp
+echo "(in-package :libndbapi)" > $clos
+cat $temp >> $clos
