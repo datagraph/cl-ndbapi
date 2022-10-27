23f88a827180d273209ff7251316fad3049380d6
Max-Gerd Retzlaff <mgr@matroid.org>
2022-10-28 01:35:21 +0200

    evolve new macro for constructor wrappers

--- src/constructors.lisp
+++ src/constructors.lisp
@@ -23,6 +23,44 @@
                             ;; HACK: this reference to instance keeps the instance from being GC'ed
                             (debug-free-thing type instance))))
 
+(defmacro make-finalize-wrapper (name type instance &rest arguments)
+  (let ((hidden-name (find-symbol (format nil "~a%" name) :ndbapi.ffi)))
+    `(defun ,name ,arguments
+       (let ((object (,hidden-name ,@arguments)))
+         (finalize-thing object ,type (or ,instance ,(first arguments)))
+         object))))
+
+(make-finalize-wrapper ndbapi.ffi::new-ndb/swig-1 :cluster-connection nil cluster-connection database-name)
+
+(defmacro make-finalize-wrapper (name instance &rest arguments)
+  (let ((hidden-name (find-symbol (format nil "~a%" name) :ndbapi.ffi))
+        (type (intern (symbol-name (first arguments)) :keyword)))
+    `(defun ,name ,arguments
+       (let ((object (,hidden-name ,@arguments)))
+         (finalize-thing object ,type (or ,instance ,(first arguments)))
+         object))))
+
+(defmacro make-finalize-wrapper (name (&rest params) &optional first-param)
+  (let ((hidden-name (find-symbol (format nil "~a%" name) :ndbapi.ffi))
+        (type (intern (symbol-name (or first-param (first params))) :keyword)))
+    `(defun ,name ,(if first-param (cons first-param params) params)
+       (let ((object (,hidden-name ,@params)))
+         (finalize-thing object ,type ,(or first-param (first params)))
+         object))))
+
+(make-finalize-wrapper ndbapi.ffi::new-ndb/swig-1 (cluster-connection database-name))
+(make-finalize-wrapper ndbapi.ffi::new-ndb-cluster-connection/swig-0 (connect-string) ndb-init)
+
+(defmacro make-finalize-wrapper (name (&rest params) &key (first-param (first params) first-param-p))
+  (let ((hidden-name (find-symbol (format nil "~a%" name) :ndbapi.ffi))
+        (type (intern (symbol-name first-param) :keyword)))
+    `(defun ,name ,(if first-param-p (cons first-param params) params)
+       (let ((object (,hidden-name ,@params)))
+         (finalize-thing object ,type ,first-param)
+         object))))
+
+(make-finalize-wrapper ndbapi.ffi::new-ndb/swig-1 (cluster-connection database-name))
+(make-finalize-wrapper ndbapi.ffi::new-ndb-cluster-connection/swig-0 (connect-string) :first-param ndb-init)
 
 (defun ndbapi.ffi::new-ndb/swig-1 (cluster-connection database-name)
   (let ((ndb (ndbapi.ffi::new-ndb/swig-1% cluster-connection database-name)))
@@ -65,3 +103,10 @@
   (let ((cluster-connection (ndbapi.ffi::new-ndb-cluster-connection/swig-4% connectstring main_connection force_api_nodeid)))
     (finalize-thing cluster-connection :ndb-init ndb-init)
     cluster-connection))
+
+
+;; add ndb-init wrapper as first argument to keep reference in finalizer for the ndb-interpreted-code created
+(defun ndbapi.ffi::new-ndb-interpreted-code/swig-0 (ndb-init table buffer buffer-word-size)
+  (let ((thing (ndbapi.ffi::new-ndb-interpreted-code/swig-0% table buffer buffer-word-size)))
+    (finalize-thing thing :ndb-init ndb-init)
+    thing))
