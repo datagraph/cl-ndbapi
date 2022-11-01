38e965649b1c792f3d62a151de23865cd27e5e0f
Max-Gerd Retzlaff <mgr@matroid.org>
2022-11-01 13:34:16 +0100

    replace ndbapi:with-ndb-transaction-scan-index by ndbapi:with-ndb-transaction-get-ndb-index-scan-operation and ndbapi:ndb-index-scan-operation-read-tuples that is from recattr to record api
    cannot use set bounds anymore because of:
    
    set-bound() failed: Error with code 4284: Cannot mix NdbRecAttr and NdbRecord methods in one operation

--- examples/ndbapi-scan-count.lisp
+++ examples/ndbapi-scan-count.lisp
@@ -58,56 +58,40 @@
                        (index-default-record (ndbapi:index-get-default-record index))
                        (table-default-record (ndbapi:table-get-default-record table))
                        (scan-flags '(:+SF-ORDER-BY+ :+SF-MULTI-RANGE+)))
-                  (ndbapi:with-foreign-struct (scan-options (list :options-present '(:+SO-SCANFLAGS+
-                                                                                     :+SO-INTERPRETED+)
-                                                                  :scan-flags scan-flags
-                                                                  :interpreted-code (ndbapi.types::foreign-pointer code))
-                                                            '(:struct ndbapi:scan-options))
-                    ;;(break "~a" (cffi:convert-from-foreign scan-options '(:struct ndbapi:scan-options)))
+                  (ndbapi:with-ndb-transaction-get-ndb-index-scan-operation (scan (transaction index)
+                                                                             ())
+                    (ndbapi:ndb-index-scan-operation-read-tuples scan :+LM-READ+ scan-flags)
+                    (ndbapi:ndb-scan-operation-set-interpreted-code scan code)
 
-                    ;; Hitting a wall now as I cannot read the "pseudo column" NdbDictionary::Column::ROW_COUNT
-                    ;; which is supposed to return the "Read row count (committed)" with the newer record API.
+                    ;; set bounds for scan
+                    (ndb.quads:with-foreign-quad (low-quad (ndb.quads:list-to-quad low))
+                      (ndb.quads:with-foreign-quad (high-quad (ndb.quads:list-to-quad high))
+                        (ndbapi:with-foreign-struct (bound (list :low-key low-quad
+                                                                 :low-key-count (length low)
+                                                                 :low-inclusive low-inclusive
+                                                                 :high-key high-quad
+                                                                 :high-key-count (length high)
+                                                                 :high-inclusive high-inclusive
+                                                                 :range-no 0)
+                                                           '(:struct ndbapi:index-bound))
+                          ;;(ndbapi:ndb-index-scan-operation-set-bound scan index-default-record bound)
+                          (ndbapi:ndb-transaction-execute transaction :+NO-COMMIT+))))
 
-                    (ndbapi:with-ndb-transaction-scan-index (scan (transaction
-                                                                      index-default-record
-                                                                      table-default-record
-                                                                      :+LM-READ+
-                                                                      (cffi:null-pointer)
-                                                                      (cffi:null-pointer)
-                                                                      scan-options
-                                                                      (cffi:foreign-type-size
-                                                                       '(:struct ndbapi:scan-options)))
-                                                             (t))
+                    ;; // Check rc anyway
 
-                      ;; set bounds for scan
-                      (ndb.quads:with-foreign-quad (low-quad (ndb.quads:list-to-quad low))
-                        (ndb.quads:with-foreign-quad (high-quad (ndb.quads:list-to-quad high))
-                          (ndbapi:with-foreign-struct (bound (list :low-key low-quad
-                                                                   :low-key-count (length low)
-                                                                   :low-inclusive low-inclusive
-                                                                   :high-key high-quad
-                                                                   :high-key-count (length high)
-                                                                   :high-inclusive high-inclusive
-                                                                   :range-no 0)
-                                                             '(:struct ndbapi:index-bound))
-                            (ndbapi:ndb-index-scan-operation-set-bound scan index-default-record bound)
-                            (ndbapi:ndb-transaction-execute transaction :+NO-COMMIT+))))
-
-                      ;; // Check rc anyway
-
-                      ;; do scan and print
-                      (format t "~&table: ~a" table-name)
-                      (format t "~&columns:   ~{~12@a~^, ~}" (list :subject :predicate :object :graph))
-                      (cffi:with-foreign-pointer (row-data 1)
-                        (loop for rc = (ndbapi:ndb-scan-operation-next-result scan row-data t nil)
-                              for j from 0
-                              while (zerop rc)
-                              for row = (ndb.quads:convert-foreign-quad (cffi:mem-aref row-data :pointer))
-                              do (format t "~&row ~5d: ~{~12d~^, ~}" j (ndb.quads:quad-to-list row))
-                              finally (assert (= rc 1)
-                                              ()
-                                              "scan-operation-next-result() failed: ~a"
-                                              (ndbapi:get-ndb-error transaction #'ndbapi:ndb-transaction-get-ndb-error))))))))))))
+                    ;; do scan and print
+                    (format t "~&table: ~a" table-name)
+                    (format t "~&columns:   ~{~12@a~^, ~}" (list :subject :predicate :object :graph))
+                    (cffi:with-foreign-pointer (row-data 1)
+                      (loop for rc = (ndbapi:ndb-scan-operation-next-result scan row-data t nil)
+                            for j from 0
+                            while (zerop rc)
+                            for row = (ndb.quads:convert-foreign-quad (cffi:mem-aref row-data :pointer))
+                            do (format t "~&row ~5d: ~{~12d~^, ~}" j (ndb.quads:quad-to-list row))
+                            finally (assert (= rc 1)
+                                            ()
+                                            "scan-operation-next-result() failed: ~a"
+                                            (ndbapi:get-ndb-error transaction #'ndbapi:ndb-transaction-get-ndb-error)))))))))))
       (sleep 10))))
 
 #+(or)
