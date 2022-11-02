8d831802052d33c49017e23e928bf35b2a9d21f1
Max-Gerd Retzlaff <mgr@matroid.org>
2022-11-02 20:33:50 +0100

    improve simple overloading by arity: add error checking that arity is unique
    and throw an error if it is not

--- src/overloading.lisp
+++ src/overloading.lisp
@@ -16,22 +16,33 @@
 ;;(overload-function #.(ndbapi.ffi::swig-lispify "Ndb_init" 'function))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun list-functions-by-arity (base-symbol package)
-    (loop for i from 0 below 100
-          for sym = (find-symbol (format nil "~s~a~d" base-symbol
-                                         "/SWIG-"
-                                         i) package)
-          while sym
-          for fn = sym
-          for arity = (length (sb-introspect:function-lambda-list fn))
-          collect (cons arity fn))))
+  (defun list-functions-by-arity (base-symbol package &key error-on-duplicates)
+    "second value is t if there are duplicates"
+    (let ((result (loop for i from 0 below 100
+                        for sym = (find-symbol (format nil "~s~a~d" base-symbol
+                                                       "/SWIG-"
+                                                       i) package)
+                        while sym
+                        for fn = sym
+                        for arity = (length (sb-introspect:function-lambda-list fn))
+                        collect (cons arity fn))))
+      (when error-on-duplicates
+        (let ((unique (remove-duplicates result :key #'car)))
+          (assert (= (length result)
+                     (length unique))
+                  ()
+                  "Arity of function ~a::~a is not unique.~&Duplicates: ~a"
+                  package base-symbol
+                  (set-difference result unique))))
+      result)))
 
 (defmacro overload-function-by-arity (name &optional (package 'ndbapi.ffi))
   "simple dispatch on number of arguments
 WARNING: this only works when there are no multiple functions with the same arity!"
   `(defun ,name (&rest args)
      (let* ((arity (length args))
-            (fn (cdr (assoc arity ',(list-functions-by-arity name package)))))
+            ;; could be improved by binary search...
+            (fn (cdr (assoc arity ',(list-functions-by-arity name package :error-on-duplicates t)))))
        (assert fn
                (fn)
                "no variant of function ~a with arity ~a: ~a" ',name arity args)
@@ -43,3 +54,5 @@ WARNING: this only works when there are no multiple functions with the same arit
 (overload-function-by-arity #.(ndbapi.ffi::swig-lispify "NdbTransaction_scanIndex" 'function))
 (overload-function-by-arity #.(ndbapi.ffi::swig-lispify "NdbScanOperation_close" 'function))
 (overload-function-by-arity #.(ndbapi.ffi::swig-lispify "NdbScanOperation_nextResult" 'function))
+;; no unique arity for:
+;; (overload-function-by-arity #.(ndbapi.ffi::swig-lispify "NdbIndexScanOperation_readTuples" 'function))
