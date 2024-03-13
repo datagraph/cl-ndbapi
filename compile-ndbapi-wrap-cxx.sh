1bb959ed155a31e954c6bbac7de0149038dd1652
Max-Gerd Retzlaff <mgr@matroid.org>
2024-03-13 16:53:29 +0100

    wrap scripts for readability

--- compile-ndbapi-wrap-cxx.sh
+++ compile-ndbapi-wrap-cxx.sh
@@ -1,1 +1,16 @@
-g++ -std=c++17 -fPIC -I./rondb/storage/ndb/include/ -I./rondb/prod_build/storage/ndb/include/ -I./rondb/storage/ndb/include/ndbapi/ -I./rondb/storage/ndb/src/ndbapi/ -I./rondb/include -I./rondb/prod_build/include -I./rondb/storage/ndb/include/kernel -I./rondb/storage/ndb/include/util -I./rondb/storage/ndb/include/transporter  -I./rondb/storage/ndb/include/portlib -I./rondb/ -I./rondb/storage/ndb/include/mgmapi -I./rondb/storage/ndb/include/debugger -o ndbapi_wrap.o -c src/ndbapi_wrap.cxx
+g++ -std=c++17 -fPIC \
+    -I./rondb/storage/ndb/include/ \
+    -I./rondb/prod_build/storage/ndb/include/ \
+    -I./rondb/storage/ndb/include/ndbapi/ \
+    -I./rondb/storage/ndb/src/ndbapi/ \
+    -I./rondb/include \
+    -I./rondb/prod_build/include \
+    -I./rondb/storage/ndb/include/kernel \
+    -I./rondb/storage/ndb/include/util \
+    -I./rondb/storage/ndb/include/transporter \
+    -I./rondb/storage/ndb/include/portlib \
+    -I./rondb/ \
+    -I./rondb/storage/ndb/include/mgmapi \
+    -I./rondb/storage/ndb/include/debugger \
+    -o ndbapi_wrap.o \
+    -c src/ndbapi_wrap.cxx
--- link-ndbapi-wrap-cxx.sh
+++ link-ndbapi-wrap-cxx.sh
@@ -1,2 +1,5 @@
-g++ -shared -Wall -L./rondb/prod_build/lib  ndbapi_wrap.o  -lndbclient -ondbapi_wrap.so
-
+g++ -shared -Wall \
+    -L./rondb/prod_build/lib \
+    ndbapi_wrap.o \
+    -lndbclient \
+    -ondbapi_wrap.so
